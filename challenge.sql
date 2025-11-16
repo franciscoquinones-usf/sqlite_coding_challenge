77ddae376fbcfe21255f722558131360e88d1040
Francisco Quinones <fquinonescedeno@usf.edu>
2025-11-16 01:52:21 -0500

    Task 3

--- challenge.sql
+++ challenge.sql
@@ -20,3 +20,19 @@ JOIN orders as o
     ON oi.order_id = o.id
 GROUP BY p.category
 ORDER BY revenue DESC;
+
+-- Task 3: Employees Earnings Above their Department Average
+SELECT e.first_name, e.last_name, 
+    d.name AS department_name,
+    e.salary AS employee_salary,
+    dept.avg_salary AS department_average
+FROM employees AS e
+JOIN departments AS d
+    ON e.department_id = d.id
+JOIN (SELECT department_id, AVG(salary) AS avg_salary
+    FROM employees GROUP BY department_id)
+AS dept on e.department_id = dept.department_id
+WHERE e.salary > dept.avg_salary
+ORDER BY d.name, e.salary DESC;
+
+-- Task 4: Cities with the Most Loyal Customers
